5ea458105a708f0ab66b3e8201016afc1d7a91c1
Tobias Schlemmer <keinstein@users.sourceforge.net>
2018-01-24 15:37:41 +0100

    libunistring 0.9.7

---
+++ version.sh
@@ -0,0 +1,6 @@
+# Version number and release date.
+VERSION_NUMBER=0.9.7
+RELEASE_DATE=2016-12-02      # in "date +%Y-%m-%d" format
+
+# Version of gnulib that was used in this release.
+GNULIB_GIT_COMMIT=31a08abd323ebffea3d4fb2d5a66f801fe8b3031
